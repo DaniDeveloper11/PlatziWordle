5158bd90ff8bf83218f9947a746bc77e19423d3b
DaniDeveloper11 <angel.montes0498@gmail.com>
2023-11-29 23:45:44 -0600

    letra aparece en la primera columna primera fila

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("PlatziWordle")
+import { fromEvent } from "rxjs";
+
+const letterRows = document.getElementsByClassName('letter-row')
+const onKeyDown$ = fromEvent(document, "keydown");
+let letterIndex = 0;
+let letterRowIndex = 0;
+
+const insertLetter = {
+    next: (event) => {
+        const pressedKey = event.key.toUpperCase();
+        if(pressedKey.length === 1 && pressedKey.match(/[a-z]/i)){
+            let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+            letterBox.textContent = pressedKey;
+            letterBox.classList.add("filled-letter");
+            // console.log(event.key);
+        }
+    }
+}
+
+onKeyDown$.subscribe(insertLetter);
